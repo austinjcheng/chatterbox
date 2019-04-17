594f847ca17a64295c260d3f1daa23b8c2aa65a4
Austin Cheng <austinjcheng@gmail.com>
2019-04-17 11:03:39 -0700

    Implement Message Threading and User Input

--- Run.py
+++ Run.py
@@ -5,31 +5,36 @@ from Initialize import joinRoom
 from Settings import usernames
 import threading
 
+def refreshMessages():
+    readbuffer = ""
+    while True:
+        readbuffer = readbuffer + users[0].recv(1024)
+        temp = string.split(readbuffer, "\n")
+        readbuffer = temp.pop()
+            
+        for line in temp:
+            print(line)
+            user = getUser(line)
+            message = getMessage(line)
+            print(user + " typed :" + message)
+
 # Initialize the connection with the given parameters
 users = [] # List of logged in users
 for num, name in enumerate(usernames):
-    print("1")
     users.append(openSocket(num))
     joinRoom(users[num])
-
-readbuffer = ""
-
-
-while True:
-    readbuffer = readbuffer + users[0].recv(1024)
-    temp = string.split(readbuffer, "\n")
-    readbuffer = temp.pop()
-        
-    for line in temp:
-        print(line)
-        user = getUser(line)
-        message = getMessage(line)
-        print(user + " typed :" + message)
-        sendMessage(users[0], raw_input("Enter: "))
     
     
-    #commmandFile = open("command.txt", 'r')
-    #for line in commandFile:
-    #    if line in message:
+messageThread = threading.Thread(target=refreshMessages)
+messageThread.start()
+
+message = ""
+while message.lower() != "exit":
+    message = raw_input("Enter: ")
+    sendMessage(users[0], message)
+
+
+
+
 
             
