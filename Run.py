aa9b208929d0e483268c6e522f6d9ea1c56f28b3
Austin Cheng <austinjcheng@gmail.com>
2019-04-17 15:32:13 -0700

    Implement Key Listener
    Add key listener for switching users

--- Run.py
+++ Run.py
@@ -3,12 +3,22 @@ from Read import getUser, getMessage
 from Socket import openSocket, sendMessage
 from Initialize import joinRoom
 from Settings import usernames
+from pynput import keyboard
 import threading
+
+# Initialize the connection with the given parameters
+users = [] # List of logged in users
+for num, name in enumerate(usernames):
+    users.append(openSocket(num))
+    joinRoom(users[num])
+    
+currentUserIndex = [0]
+currentUserIndex[0] = 0
 
 def refreshMessages():
     readbuffer = ""
     while True:
-        readbuffer = readbuffer + users[0].recv(1024)
+        readbuffer = readbuffer + users[currentUserIndex[0]].recv(1024)
         temp = string.split(readbuffer, "\n")
         readbuffer = temp.pop()
             
@@ -17,13 +27,14 @@ def refreshMessages():
             user = getUser(line)
             message = getMessage(line)
             print(user + " typed :" + message)
+             
+def on_press(key):
+    if key == keyboard.Key.tab:
+        currentUserIndex[0] += 1
+        if currentUserIndex[0] >= len(users):
+            currentUserIndex[0] = 0
+        print("Switched user to: " + users[currentUserIndex[0]])
 
-# Initialize the connection with the given parameters
-users = [] # List of logged in users
-for num, name in enumerate(usernames):
-    users.append(openSocket(num))
-    joinRoom(users[num])
-    
     
 messageThread = threading.Thread(target=refreshMessages)
 messageThread.start()
@@ -31,7 +42,7 @@ messageThread.start()
 message = ""
 while message.lower() != "exit":
     message = raw_input("Enter: ")
-    sendMessage(users[0], message)
+    sendMessage(users[currentUserIndex[0]], message)
 
 
 
