2d8323d1ef1aab2caf158a7cf76dec1aff75752c
Austin Cheng <austinjcheng@gmail.com>
2019-04-26 14:43:16 -0700

    Begin GUI Implementation

--- GUI.py
+++ GUI.py
@@ -1,10 +1,41 @@
 from Tkinter import *
 
 def sendMessage():
+    sendMessage(entry.get())
+    entry.delete(0, END)
+
+root = Tk()
+
+entry = Entry(root)
+chat = Text(root, height=2, width=30)
+
+entry.grid(row=1, column=1)
+chat.grid(row=0, column=1)
+
+#entry.bind('<Tab>', hello)
+
+Button(root, text='Enter', command=sendMessage).grid(row=3, column=1, sticky=W, pady=4)
+
+mainloop( )
+
+
+
+
+
+
+
+
+
+
+
+"""
+from Tkinter import *
+
+def sendMessage():
     return
 
 root = Tk()
-root.title("Twitch Spammer")
+root.title("Chatterbox")
 
 w = Label(root, text="Hello Tkinter!")
 w.pack()
@@ -14,3 +45,4 @@ entry = Entry(root)
 Button(root, text='Enter', command=sendMessage).grid(row=0, column=1, sticky=W, pady=4)
 
 root.mainloop()
+"""
--- Run.py
+++ Run.py
@@ -5,15 +5,19 @@ from Initialize import joinRoom
 from Settings import usernames
 from pynput import keyboard
 import threading
+from Tkinter import *
 
-# Initialize the connection with the given parameters
+
+currentUserIndex = [0]
+currentUserIndex[0] = 0
+
+    # Initialize the connection with the given parameters
 users = [] # List of logged in users
 for num, name in enumerate(usernames):
     users.append(openSocket(num))
     joinRoom(users[num])
-    
-currentUserIndex = [0]
-currentUserIndex[0] = 0
+        
+
 
 def refreshMessages():
     readbuffer = ""
@@ -35,7 +39,7 @@ def on_press(key):
         if currentUserIndex[0] >= len(users):
             currentUserIndex[0] = 0
         print("Switched user to: " + users[currentUserIndex[0]])
-"""
+
 def on_press(key):
     try:
         if key == keyboard.Key.tab:
@@ -51,20 +55,39 @@ def on_release(key):
     if key == keyboard.Key.esc:
         # Stop listener
         return False
-    
-
+    """
 messageThread = threading.Thread(target=refreshMessages)
 messageThread.start()
 
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
 
-message = ""
-while message.lower() != "exit":
-    message = raw_input("Enter: ")
-    sendMessage(users[currentUserIndex[0]], message)
+def sendMessage():
+    sendMessage(entry.get())
+    entry.delete(0, END)
+
+root = Tk()
+
+entry = Entry(root)
+chat = Text(root, height=2, width=30)
+
+entry.grid(row=1, column=1)
+chat.grid(row=0, column=1)
+
+#entry.bind('<Tab>', hello)
+
+Button(root, text='Enter', command=sendMessage).grid(row=3, column=1, sticky=W, pady=4)
+
+mainloop( )
+    """
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release)
+    listener.start()
+    
+    message = ""
+    while message.lower() != "exit":
+        message = raw_input("Enter: ")
+        sendMessage(users[currentUserIndex[0]], message)
+        """
 
 
 
